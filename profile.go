4a7f5f50aa551d727b7938027b3df3dd5c4e843d
Jack Mordaunt <jackmordaunt@gmail.com>
2018-06-21 21:35:48 +1200

    [+] Optional profiling behind a build tag.

--- profile.go
+++ profile.go
@@ -5,5 +5,8 @@ package main
 import "github.com/pkg/profile"
 
 func init() {
-	profile.Start(profile.ProfilePath("./profiling"), profile.NoShutdownHook)
+	profile.Start(
+		profile.ProfilePath("./profiling"),
+		profile.MemProfile,
+	)
 }
